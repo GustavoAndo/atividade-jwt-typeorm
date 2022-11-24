3e2500fd959daa3ac83751fd6a6df93fdf86fc39
Gustavo Ando <gustavo.k.ando@gmail.com>
2022-11-24 15:52:13 -0300

    iniciando projeto

---
+++ src/data-source.ts
@@ -0,0 +1,30 @@
+import { DataSource } from "typeorm";
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const AppDataSource = new DataSource({
+    url: process.env.DB_URL,
+    // host: 'localhost',
+    // port: 5432,
+    // username: 'postgres',
+    // password: 'fatec',
+    // database: 'atividade',
+    type: "postgres",
+    synchronize: false,
+    logging: false,
+    entities: ["src/entities/*.ts"],
+    migrations: ["src/migrations/*.ts"], 
+    subscribers: [],
+    maxQueryExecutionTime: 2000 // 2 seg.
+});
+
+AppDataSource
+    .initialize()
+    .then(() => {
+        console.log("Data Source inicializado!")
+    })
+    .catch((e) => {
+        console.error("Erro na inicialização do Data Source:", e)
+    });
+
+export default AppDataSource;
